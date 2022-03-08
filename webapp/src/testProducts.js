383cdcac186bb65722817fa1cd223e0b4012a9c9
UO263595 <uo263595@uniovi.es>
2022-03-08 18:24:57 +0100

    Cambios menores

---
+++ webapp/src/testProducts.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+        id: 1,
+        name: "Maserati",
+        type: "500",
+        price: 100000
+    },
+    {
+        id: 2,
+        name: "fgs",
+        type: "200",
+        price: 200000
+    },
+    {
+        id: 3,
+        name: "gsgfd",
+        type: "300",
+        price: 300000
+    },
+    {
+        id: 4,
+        name: "gsfs",
+        type: "400",
+        price: 400000
+    },
+];
+
+export default products;
