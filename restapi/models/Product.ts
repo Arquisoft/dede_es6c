7926217d505f3c0b2c12b8d741006cb618cc3446
Luis Manuel <uo269502@uniovi.es>
2022-03-09 17:14:00 +0100

    Get Post de productos

--- restapi/models/Product.ts
+++ restapi/models/Product.ts
@@ -1,26 +1,25 @@
 import mongoose from 'mongoose';
-import {ProductType} from './../types';
 
 const { Schema } = mongoose;
 
 const productoSchema  = new Schema({
-    _id: {
-        type: String,
-        required: true
-    },
-    name:{
+  _id: {
       type: String,
-      required: false
-    },
-    type:{
-      type:String,
-      required :false
-    },    
-    price: {
-        type: Number,
-        required: true
-    }
+      required: true
+  },
+  name:{
+    type: String,
+    required: false
+  },
+  type:{
+    type:String,
+    required :false
+  },
+  price: {
+      type: Number,
+      required: true
+  }
 });
 
-const Product:mongoose.Model<ProductType> = mongoose.model('product', productoSchema);
-export default Product; 
+const productos = mongoose.model('productos', productoSchema)
+export default productos; 
