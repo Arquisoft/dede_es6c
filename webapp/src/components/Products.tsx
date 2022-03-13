384dcbabe665413f7d255178d483f7ce2794e1c2
uo269783 <uo269783@uniovi.es>
2022-03-13 23:15:51 +0100

    mostrar productos WIP
    comenté el console.log de la clase api en restapi

--- webapp/src/components/Products.tsx
+++ webapp/src/components/Products.tsx
@@ -3,14 +3,23 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Product from "./ProductCard";
 import products from "../testProducts";
+import { getProducts } from "../api/api";
+import { SharedProduct } from '../shared/shareddtypes';
 
 export default function Products() {
+
+  let promise : Promise<SharedProduct[]>
+  promise = getProducts();
+  let productos2 : SharedProduct[] = [];
+  promise.then(function(result)  {result.forEach(element => productos2.push(element)); });
+  console.log(productos2);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        {products.map((product) => (
+        {productos2.map((product) => (
           <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Product key={product.id} id={product.id} name={product.name} type={product.type} price={product.price}/>
+            <Product key={product._id} id={product._id} name={product.name} type={product.type} price={product.price}/>
           </Grid>
         ))}
       </Grid>
