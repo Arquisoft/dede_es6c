de194310c0aa6b217ff47ebd805334d1663e7c09
GermanDiaz <UO258472@uniovi.es>
2022-03-14 10:19:52 +0100

    Pequeños arreglos

--- webapp/src/shared/shareddtypes.ts
+++ webapp/src/shared/shareddtypes.ts
@@ -1,4 +1,12 @@
 export type User = {
-    name:string;
-    email:string;
-  }
+  name:string;
+  email:string;
+}
+
+
+export type ProductType = {
+  id : Number;
+  name: String;
+  price: number;
+  type : String;
+}
