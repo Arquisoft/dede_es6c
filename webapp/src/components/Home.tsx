f7cc07312a4c592286f8827ea6db6de7e9c9fcc4
UO263595 <uo263595@uniovi.es>
2022-03-16 09:49:24 +0100

    Vista home básica
    Diseño de la vista home básica y pequeña modificación en las rutas para que esta sea la vista por defecto.

--- webapp/src/components/Home.tsx
+++ webapp/src/components/Home.tsx
@@ -1,9 +1,40 @@
 import React from "react";
+import coche from '../images/coche.jpg';
 
 export default function Home() {
     return (
         <body>
-            <h1>Home</h1>
+            <div>
+                <h1>Welcome to Dede</h1>
+                <p>Sale of used vehicles, spare parts and accessories related to the automotive industry.</p>
+            </div>
+            <div>
+                <div>
+                <h4>About Us</h4>
+                <p>We are a humble company dedicated to the automotive industry. Our vision is to achieve total customer satisfaction through our services.</p>
+                </div>
+                <div>
+                <h4>Quick Tips</h4>
+                <ul>
+                    <li>Check tire pressure</li>
+                    <li>Check oil level</li>
+                    <li>Follow the maintenance instructions in the car's manual</li>
+                    <li>Wash the car regularly</li>
+                    <li>Check the condition of the lights</li>
+                    <li>Check the condition of the wheel nuts</li>
+                </ul>
+                </div>
+                <div>
+                <h4>Services</h4>
+                <ul>
+                    <li>High quality service</li>
+                    <li>Vehicles checked at 120 checkpoints</li>
+                    <li>No maintenance costs for 12 months or 15,000 Km</li>
+                    <li>Satisfaction or refund</li>
+                </ul>
+                </div>
+                <img src={coche} alt="logo" id="logo"/>
+            </div>
         </body>
     );
 }
