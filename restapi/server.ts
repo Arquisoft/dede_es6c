f2a6e6daf022b5b679a08442d2648b9ad0c5fa63
AGPAllStar <crasarionic@gmail.com>
2022-04-28 16:06:29 +0200

    Cambios arreglos errores api despliegue

--- restapi/server.ts
+++ restapi/server.ts
@@ -18,7 +18,7 @@ app.use(metricsMiddleware);
 app.use(cors());
 app.use(bp.json());
 
-app.use("/api", api)
+app.use("/", api)
 
 app.listen(port, ():void => {
     console.log('Restapi listening on '+ port);
