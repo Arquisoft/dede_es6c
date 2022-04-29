6741c3e78f490310a3d8241949ba5b3668db2622
UO275778 <79647846+AGPAllStar@users.noreply.github.com>
2022-04-29 11:46:50 +0200

    Update server.ts

--- restapi/server.ts
+++ restapi/server.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 const port: number = 5000;
 
 const options: cors.CorsOptions = {
-  origin: ['http://localhost:3000', 'http://*:5000']
+  origin: ['http://localhost:3000', 'http://localhost:5000']
 };
 
 const metricsMiddleware:RequestHandler = promBundle({includeMethod: true});
