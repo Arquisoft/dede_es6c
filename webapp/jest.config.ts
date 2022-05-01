6e5f3cd5c2cacccf30e20913449ef9af2629a00a
Luis Manuel Gonzalez Baizan <uo269502@uniovi.es>
2022-05-01 14:14:29 +0200

    Coverage especificado
    Especificación sobre que documentos tiene que realizar cobertura

--- webapp/jest.config.ts
+++ webapp/jest.config.ts
@@ -2,6 +2,8 @@ export default {
     transform: {
         "^.+\.tsx?$": "ts-jest"
     },
+    collectCoverage: true,
+    collectCoverageFrom: ["**/*.tsx"],
     testMatch: ["*/steps/.ts"],
     "testTimeout": 15000,
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
